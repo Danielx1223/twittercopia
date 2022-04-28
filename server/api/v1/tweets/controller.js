dcb69a193cea24f7a885dd1ba17fcd69e1356345
Daniel Nieto <ndanielx@gmail.com>
2022-04-27 22:17:26 -0500

    completo

--- server/api/v1/tweets/controller.js
+++ server/api/v1/tweets/controller.js
@@ -1,30 +1,45 @@
+const tweets = [];
+
 exports.all = (req, res, next) => {
   const { query = {} } = req; // metiendo valores
-  const { limit = 100, skip = 0 } = query;
   res.json({
-    limit,
-    skip,
+    data: tweets,
+    included: {
+      query,
+    },
   });
 };
 
 // Mostrando la info que coloco en postmant desde body.
 exports.create = (req, res, next) => {
   const { body = {} } = req;
+  tweets.push(body);
   res.json({
-    body,
+    data: body,
   });
 };
 
 exports.read = (req, res, next) => {
   const { params = {} } = req;
+  const { id = '' } = params;
+
+  const data = tweets.find(function (item) {
+    return item.id === id;
+  });
   res.json({
-    id: params.id, // Guardo los valores del id que ponga en la URL en la variable params
+    data: {
+      id: params.id, // Guardo los valores del id que ponga en la URL en la variable params
+    },
   });
 };
 
 exports.update = (req, res, next) => {
+  const { body = {}, params = {} } = req;
   res.json({
-    message: 'List of Tweets UPDATE',
+    data: body,
+    included: {
+      params,
+    },
   });
 };
 
